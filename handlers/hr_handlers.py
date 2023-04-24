094a517b9977832663ff42250d8cf650f46d1b4e
Lyudmila Churina <notyourbaryga@gmail.com>
2023-04-24 23:46:20 +0500

    commit

--- handlers/hr_handlers.py
+++ handlers/hr_handlers.py
@@ -1,12 +1,14 @@
 from aiogram import Router, F
+from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 from lexicon.lexicon_ru import LEXICON_RU
-from filters.hr_filter import IsHR
+#from filters.hr_filter import IsHR
 
+from models.base import base
 
 router: Router = Router()
-router.message.filter(IsHR())
+router.message.filter(base.get_user(F.text) is not None)
 
 @router.message(F.text == LEXICON_RU.see_responses)
 async def choose_hr_role(message: Message):
